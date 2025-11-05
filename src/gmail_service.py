8c73802cc97e42db26fd5d83dec03c0dd695b046
Sneha Patani <snehapatani@gmail.com>
2025-11-05 13:11:16 -0800

    Return creds

--- src/gmail_service.py
+++ src/gmail_service.py
@@ -66,6 +66,7 @@ def authenticate_gmail_cloud():
         creds = flow.credentials
         st.session_state["gmail_creds"] = creds
         st.experimental_rerun()  # Refresh page after authentication
+        return creds
 
 def get_emails(service, query: str, max_results=5):
     """
